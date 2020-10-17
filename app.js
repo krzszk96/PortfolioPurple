2910c23665fe16cce9640efa011834c668ca7c0b
krzysztof <krzszk96@gmail.com>
2020-10-17 18:26:50 +0200

    footer and scroll btn done

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const burgerLine = document.querySelectorAll('.hamburger .line');
+const scroll = document.querySelector('.scroll-button');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   burgerLine.forEach(line => {
     line.classList.toggle('toggle');
+    line.classList.toggle('open');
   })
 });
 navLinks.addEventListener('click', () => {
@@ -16,3 +18,13 @@ navLinks.addEventListener('click', () => {
   })
 });
 
+var clicked = false;
+scroll.addEventListener('click', () => {
+  
+  if(clicked){
+    window.location.href = '#contact';
+  }else{
+    window.location.href = '#mywork';
+  }
+  clicked = !clicked;
+});
